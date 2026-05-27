d81cfe5c97807bc84db93a0c7cc1d0a5fd073cad
TheLastGimbus <mateusz.soszynski@tuta.io>
2026-05-27 11:48:11 +0200

    blink

--- methylphenidate_vivado/methylphenidate_vivado.srcs/sources_1/new/main.vhd
+++ methylphenidate_vivado/methylphenidate_vivado.srcs/sources_1/new/main.vhd
@@ -1,43 +1,28 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05/27/2026 11:31:48 AM
--- Design Name: 
--- Module Name: main - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity main is
---  Port ( );
+  Port (
+    CLK : in std_logic;
+    LED : out std_logic_vector(3 downto 0)
+  );
 end main;
 
 architecture Behavioral of main is
 
+    signal state : std_logic := '0';
+
 begin
 
-
+    blink : process(CLK) begin
+        if(rising_edge(CLK)) then
+            state <= not state;
+        else
+            state <= state;
+        end if;
+    end process blink;
+    
+    LED(0) <= state;
+    
 end Behavioral;
